3c9e4d0c8a0d8d5514036a7916315b2d94c40f0c
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-30 21:46:25 +0545

    auth

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+
+
+    #authentication
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/signup/', views.signup_view, name='signup'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -1,4 +1,106 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
+@login_required
 def dashboard(request):
     return render(request, 'main/dashboard.html')
+
+
+
+def login_view(request):
+
+    if request.method == "POST":
+        data = request.POST
+
+        username = data['username']
+        password = data['password']
+
+        # user = User.objects.filter(username=username).first()
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            # if user.check_password(password):  #you can use this too
+            login(request, user)
+            return redirect('main:dashboard')
+            
+        else:
+            messages.error(request, 'Invalid Credentials')
+
+            return redirect("main:login")
+
+
+    return render(request, 'main/login.html')
+
+
+
+
+
+
+
+def signup_view(request):
+
+    if request.method == "POST":
+        data = request.POST
+
+        username = data['username']
+
+        #check if username already exists
+        try:
+            User.objects.get(username=username)
+
+            #generate auto username using random module and use your own creative logic
+
+            messages.error(request, f'User with {username} already exists, try another username')
+        except User.DoesNotExist:
+             #username validation    
+            if len(username) < 6:
+                messages.error(request, 'Username must be at least 6 characters long')
+
+                context = {
+                    'old_data': data,
+                }
+                return render(request, 'main/signup.html', context)
+            
+
+            #password length validation
+            if len(data['password']) < 6:
+                messages.error(request, 'Password must be at least 6 characters long')
+
+                context = {
+                    'old_data': data,
+                }
+                return render(request, 'main/signup.html', context)
+            
+            else:
+                #password match validation
+                if data['password'] != data['confirm_password']:
+                    messages.error(request, 'Password and Confirm Password do not match')
+
+                    context = {
+                        'old_data': data,
+                    }
+                    return render(request, 'main/signup.html', context)
+                
+            #user data is valid, lets create user in database
+
+            user = User.objects.create_user(data['username'], data['email'], data['password'])
+
+            # user.save()s
+
+            messages.success(request, f'Account for {user.username} created successfully')
+
+            return redirect("main:login")
+
+
+
+    return render(request, 'main/signup.html')
+
+
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("main:login")
